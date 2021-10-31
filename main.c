0d04fca3bc5371d44cfc3045ab59afbc9db88ec1
Arek001 <59519837+Arek001@users.noreply.github.com>
2021-10-31 20:28:49 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int liczba=10;
+    printf("Liczba %d\n",liczba);
+    printf("Jej wartosc podwojona jest rowna %d\n",liczba*2);
+    printf("Oraz kwadrat tej liczby jest rowny %d\n",liczba*liczba);
+    return 0;
+}
